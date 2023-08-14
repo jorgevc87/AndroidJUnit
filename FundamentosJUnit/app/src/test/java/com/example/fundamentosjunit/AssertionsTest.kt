085e70667c5fbf65177f5e5f320adda17787ba18
JorgeVC <test@test.com>
2023-08-14 10:39:58 -0500

    assertequals y assertnotequals

--- FundamentosJUnit/app/src/test/java/com/example/fundamentosjunit/AssertionsTest.kt
+++ FundamentosJUnit/app/src/test/java/com/example/fundamentosjunit/AssertionsTest.kt
@@ -8,9 +8,24 @@ class AssertionsTest {
     @Test
     fun getArrayTest() {
         val assertions = Assertions()
-        //val array = arrayOf(21, 117) // Estes es el valor esperado
-        val array = arrayOf(21, 11) // Estes es el valor esperado
+        val array = arrayOf(21, 117) // Estes es el valor esperado
+        //val array = arrayOf(21, 11) // Estes es el valor esperado
 
-        assertArrayEquals(array, assertions.getLuckyNumbers())
+        assertArrayEquals(
+            "Mensaje personalizado de error en testing",
+            array,
+            assertions.getLuckyNumbers()
+        )
+    }
+
+    @Test
+    fun getNameTest() {
+        val assertions = Assertions()
+
+        val name = "Jorge Luis"
+        val otherName = "Max"
+
+        assertEquals("El nombre no coincide con el esperado", name, assertions.getName())
+        assertNotEquals("El nombre no coincide con el esperado", otherName, assertions.getName())
     }
 }
